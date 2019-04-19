d4d340b6628b5035991ffe7608d96f271c76992f
Henrik Riechert <henrik@medienbucht.de>
2019-04-19 13:10:19 +0200

    an afternoons work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './assets/styles/styles.scss';
+import Typed from 'typed.js';
+
+const config = {
+    
+}
+const options = {
+strings: ['Websites', 'Apps', 'Webshops', 'Blogs', 'eBooks', 'Templates', 'Themes'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2500,
+    loop: true
+}
+
+const typed = new Typed("#typedjs", options);
+
 console.log('this is the webpack entry point');
