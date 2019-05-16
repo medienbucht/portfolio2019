232c948f1204df02b04fcc01d2acfd4b4fe39324
Henrik Riechert <henrik@medienbucht.de>
2019-05-16 12:46:38 +0200

    added spam filter to contact form

--- src/contact.php
+++ src/contact.php
@@ -2,9 +2,16 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$pos1 = strpos($message, 'href');
+$pos2 = strpos($message, 'porn');
+$pos3 = strpos($message, 'casino');
+$spam = "";
+if($pos1 === true | $pos2 === true |$pos3 === true){
+    $spam = "SPAM: ";
+}
 $formcontent=" From: $name \n  Message: $message";
 $recipient = "henrik@medienbucht.de";
-$subject = "medienbucht.de Kontaktformular";
+$subject = $spam."medienbucht.de Kontaktformular";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Hoppla, beim Senden Ihrer Nachricht lief etwas schief. Bitte versuchen Sie es noch einmal oder nutzen die anderen Kontaktmöglichkeiten.");
 echo "Vielen Dank für Ihre Nachricht!";
