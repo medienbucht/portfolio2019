6a14fd4057f14ccaa66ed2273e90f75f45ceff88
Henrik Riechert <henrik@medienbucht.de>
2019-05-16 14:18:24 +0200

    cleaned up

--- src/assets/js/o-introducing.js
+++ src/assets/js/o-introducing.js
@@ -1,3 +1,2 @@
 import attachClassOnScroll from './scrollAnim';
 attachClassOnScroll('.o-introducing__content p','.m-gitlink','visible');
-attachClassOnScroll('.o-introducing .a-heading_second','.o-introducing .a-heading_second','anim-scrollIntoView');
